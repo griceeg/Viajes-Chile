bcbcdc6b70823c52bc51d01172deb8bb7ead43cf
griceeg <gricel_205@hotmail.com>
2024-03-12 19:40:50 -0300

    agregando últimos estilos en css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
 /* Función para smoothscroll de navbar */
-$(document).ready(function(){
-    $("nav a").on('click', function(event) {
-     if (this.hash !== "") {
-       event.preventDefault();
-       var hash = this.hash;
-   
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1000, function(){
-   
-        window.location.hash = hash;
-        });
-       }
-     });
-   });
-   
+$(document).ready(function () {
+    $("nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
 
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
 
 /* Para tooltip */
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
