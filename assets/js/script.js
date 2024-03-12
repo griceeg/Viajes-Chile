5dda9c0e6b868f0658c94454f740f86327197b6b
griceeg <gricel_205@hotmail.com>
2024-03-12 19:16:52 -0300

    agregado estilos en css y funciones en js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/* Función para smoothscroll de navbar */
+$(document).ready(function(){
+    $("nav a").on('click', function(event) {
+     if (this.hash !== "") {
+       event.preventDefault();
+       var hash = this.hash;
+   
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+   
+        window.location.hash = hash;
+        });
+       }
+     });
+   });
+   
+
+
+/* Para tooltip */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
